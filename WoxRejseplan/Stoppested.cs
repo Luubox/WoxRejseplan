ea6272d6f503f2cc7827e9e0d3950de8071a0f3c
J Nielsen <buhros@hotmail.com>
2019-02-08 12:42:14 +0100

    aebleflaesk

---
+++ WoxRejseplan/Stoppested.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoxRejseplan
+{
+    class Stoppested
+    {
+        private int _stoppestedId;
+        private string _stoppestedName;
+        private int _stoppestedCoordX;
+        private int _stoppestedCoordY;
+
+        public int StoppestedId { get => _stoppestedId; set => _stoppestedId = value; }
+        public string StoppestedName { get => _stoppestedName; set => _stoppestedName = value; }
+        public int StoppestedCoordX { get => _stoppestedCoordX; set => _stoppestedCoordX = value; }
+        public int StoppestedCoordY { get => _stoppestedCoordY; set => _stoppestedCoordY = value; }
+
+        public Stoppested(int stoppestedId, string stoppestedName, int stoppestedCoordX, int stoppestedCoordY)
+        {
+            _stoppestedId = stoppestedId;
+            _stoppestedName = stoppestedName;
+            _stoppestedCoordX = stoppestedCoordX;
+            _stoppestedCoordY = stoppestedCoordY;
+        }
+    }
+}
